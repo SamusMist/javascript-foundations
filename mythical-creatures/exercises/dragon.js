04ff443f90bcd4636f9b345354cd07ddd313f5e5
Sam Mistele <smmstl0@gmail.com>
2021-12-09 16:48:56 -0700

    Complete dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,19 @@
+class Dragon {
+  constructor(name, rider) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.isHungry = 0;
+  };
+  greet() {
+    return `Hi, ${this.rider}!`;
+  };
+  eat() {
+    this.isHungry++
+    if (this.isHungry > 2) {
+      this.hungry = false;
+    };
+  };
+}
 
+module.exports = Dragon;
