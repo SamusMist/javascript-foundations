3ae0b0851ab11d8b7b2360f57d0f59001a9d0226
Sam Mistele <smmstl0@gmail.com>
2021-12-09 16:42:16 -0700

    Complete unicorn.js

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,8 +1,16 @@
 class Unicorn {
-  constructor(name, color) {
+  constructor(name, color = 'white') {
     this.name = name;
     this.color = color;
   }
+  isWhite() {
+    if (this.color !== 'white') {
+      return false;
+    };
+  }
+  says(saying) {
+    return `**;* ${saying} *;**`;
+  };
 }
 
 module.exports = Unicorn;
