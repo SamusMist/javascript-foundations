7944cefee2119dfaffbe6b3ef5d25a0820898944
Sam Mistele <smmstl0@gmail.com>
2021-12-09 17:38:45 -0700

    Add curse, rob ship method

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -2,7 +2,11 @@ class Pirate {
   constructor(name, job) {
     this.name = name;
     this.job = job || 'scallywag';
-
-  }
+    this.cursed = false;
+  };
+  robShip() {
+    return `YAARRR!`;
+  };
 }
+
 module.exports = Pirate;
