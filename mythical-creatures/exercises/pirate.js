0f4e1233bbb4801654e68a2652d3b8d6f25c2341
Sam Mistele <smmstl0@gmail.com>
2021-12-09 17:49:13 -0700

    Add gold counter, curse

--- mythical-creatures/exercises/pirate.js
+++ mythical-creatures/exercises/pirate.js
@@ -3,8 +3,14 @@ class Pirate {
     this.name = name;
     this.job = job || 'scallywag';
     this.cursed = false;
+    this.booty = 0;
   };
   robShip() {
+    if (this.booty >= 500) {
+      this.cursed = true;
+      return `ARG! I've been cursed!`;
+    };
+    this.booty += 100;
     return `YAARRR!`;
   };
 }
