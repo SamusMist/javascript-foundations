6cff42de43088627c80680fc0d385df39e6e5406
Sam Mistele <smmstl0@gmail.com>
2021-12-10 09:29:23 -0700

    Add incantation method

--- mythical-creatures/exercises/magician.js
+++ mythical-creatures/exercises/magician.js
@@ -2,7 +2,10 @@ class Magician {
   constructor(magicianObject) {
     this.name = `The Great ${magicianObject.name}`;
     this.assistant = magicianObject.assistant;
-    this.favoriteAccessory = 'top hat'
+    this.favoriteAccessory = magicianObject.clothing || 'top hat';
+  };
+  performIncantation(incantation) {
+    return `${incantation.toUpperCase()}!`;
   };
 }
 
