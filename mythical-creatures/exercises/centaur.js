34c7c2870f38393b1208c38bcae79560a809167c
Sam Mistele <smmstl0@gmail.com>
2021-12-14 17:21:20 -0700

    Add methods and finish centaur.js

--- mythical-creatures/exercises/centaur.js
+++ mythical-creatures/exercises/centaur.js
@@ -3,12 +3,59 @@ class Centaur {
     this.name = newObject.name;
     this.breed = newObject.type;
     this.cranky = false;
+    this.standing = true;
+    this.crankyCounter = 0;
+    this.layingDown = false;
   };
   shootBow() {
+    this.crankyCounter++
+    if (this.crankyCounter > 2) {
+      this.cranky = true;
+    };
+    if (this.cranky === true) {
+      return 'NO!';
+    };
+    if (this.layingDown === true) {
+      return 'NO!';
+    };
     return 'Twang!!!';
   };
   run() {
-    return 'Clop clop clop clop!!!';
+    this.crankyCounter++
+    if (this.crankyCounter > 2) {
+      this.cranky = true;
+  };
+  if (this.cranky === true) {
+    return 'NO!';
+  };
+  if (this.layingDown === true) {
+    return 'NO!';
+  };
+  return 'Clop clop clop clop!!!';
+  };
+  sleep() {
+  if (this.standing === true) {
+    return 'NO!';
+    };
+  if (this.layingDown === true) {
+    this.crankyCounter = 0;
+    this.cranky = false;
+    return 'ZZZZ'
+    };
+  };
+  layDown() {
+    this.standing = false;
+    this.layingDown = true;
+  };
+  standUp() {
+    this.standing = true;
+    this.layingDown = false;
+  };
+  drinkPotion() {
+    if (this.standing === false) {
+      return `Not while I'm laying down!`;
+    };
+    this.cranky = false;
   };
 }
 
