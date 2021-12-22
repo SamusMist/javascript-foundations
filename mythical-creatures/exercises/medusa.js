7b16761f6dfd050caeed82cf9534c795391a7f44
Sam Mistele <smmstl0@gmail.com>
2021-12-22 09:12:15 -0700

    Finish medusa.js

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,12 +1,25 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
 class Medusa {
   constructor(name) {
     this.name = name;
     this.statues = [];
   };
-  gazeAtVictim(victim) {
-    this.statues.push(victim);
+  gazeAtVictim(newVictim) {
+    var firstVictim = {};
+    var backToPerson;
+
+    if (this.statues.length < 3) {
+      this.statues.push(new Statue(newVictim.name));
+    } else if (this.statues.length === 3) {
+      firstVictim = this.statues[0];
+      backToPerson = new Person(firstVictim.name);
+      this.statues.shift();
+      this.statues.push(new Statue(newVictim.name));
+      backToPerson.mood = 'relieved';
+      return backToPerson;
+    };
   };
 }
 
