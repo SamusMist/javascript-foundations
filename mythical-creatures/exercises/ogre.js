9017130df72fad157b50d16e4c0e1e0763c20d75
Sam Mistele <smmstl0@gmail.com>
2021-12-22 11:55:08 -0700

    Finish ogre.js

--- mythical-creatures/exercises/ogre.js
+++ mythical-creatures/exercises/ogre.js
@@ -2,7 +2,23 @@ class Ogre {
   constructor(ogreObject) {
     this.name = ogreObject.name;
     this.home = ogreObject.abode || 'Swamp';
+    this.swings = 0;
   };
-}
+  encounter(dude) {
+    dude.encounterCounter += 1;
+    if (dude.noticesOgre()) {
+      this.swingAt();
+    };
+    if (dude.encounterCounter % 2 === 0) {
+      dude.knockedOut = true;
+    };
+  };
+  swingAt() {
+    this.swings += 1;
+    };
+    apologize(dude) {
+      dude.knockedOut = false;
+    };
+  };
 
 module.exports = Ogre;
